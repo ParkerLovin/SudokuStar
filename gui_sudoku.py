b4dcb5ef60b78ff76fb8aa77bbde81303cb0002f
Parker Lovin <pblovin42@tntech.edu>
2025-01-24 16:23:32 -0500

    Created gui_sudoku.py

---
+++ gui_sudoku.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import tkinter as tk
+
+cells = []
+master = tk.Tk()
+
+def save_cells():	# TEMP
+	array = []
+	for cell in cells:
+		content = cell.get()
+		if not content in ["1", "2", "3", "4", "5", "6", "7", "8", "9", ""]:
+			text_var = tk.StringVar()
+			text_var.set("Error: please ensure that each square contains only one digit 1-9.")
+			error_label = tk.Label(master, textvariable=text_var, fg="red")
+			error_label.grid(row=11, columnspan=10)
+			return
+		else:
+			if content == "":
+				array.append(" ")
+			else:
+				array.append(content)
+	print(array)
+	
+			
+for row in range(9):
+	for col in range(9):
+		cell = tk.Entry(master, width=2, font=("Arial", 50), justify="center")
+		cell.grid(row=row, column=col)
+		cells.append(cell)
+save_button = tk.Button(master, text="Save Grid", command=save_cells)
+save_button.grid(row = 10, columnspan=10)
+master.title("SudokuSuperstar")
+
+master.mainloop()
